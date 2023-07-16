c39feddc9deb671dd89d95edc5e0158c07b88111
mena <mena.rizkalla1@gmail.com>
2023-07-16 22:44:37 +0300

    domain module

---
+++ domain/src/main/java/com/example/domain/repo/MealsRepo.kt
@@ -0,0 +1,9 @@
+package com.example.domain.repo
+
+import com.example.domain.entity.CategoryResponse
+
+interface MealsRepo {
+
+    fun getMealsFromRemote() : CategoryResponse
+
+}
---
+++ domain/src/main/java/com/example/domain/usecase/GetMeals.kt
@@ -0,0 +1,8 @@
+package com.example.domain.usecase
+
+import com.example.domain.repo.MealsRepo
+
+class GetMeals(private val mealsRepo: MealsRepo) {
+
+    suspend operator fun invoke() = mealsRepo.getMealsFromRemote()
+}
