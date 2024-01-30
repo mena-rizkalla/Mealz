45566284dcb622a3d6f0ffc8a679fa5489607c11
mena <mena.rizkalla1@gmail.com>
2024-01-30 05:14:38 +0200

    add Meals viewModel

---
+++ app/src/main/java/com/example/mealz/MealsViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.mealz
+
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.entity.CategoryResponse
+import com.example.domain.usecase.GetMeals
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.lang.Exception
+import javax.inject.Inject
+
+@HiltViewModel
+class MealsViewModel @Inject constructor( private val getMealsUseCase: GetMeals) : ViewModel() {
+
+    private val _categories : MutableStateFlow<CategoryResponse?> = MutableStateFlow(null)
+    val categories : StateFlow<CategoryResponse?> = _categories
+    fun getMeals() {
+        viewModelScope.launch{
+            try {
+                _categories.value = getMealsUseCase()
+            }catch (ex : Exception){
+
+            }
+
+        }
+    }
+}
